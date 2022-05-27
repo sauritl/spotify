d32a96aed499883ea2b8f5ad4655ccbf948f1fd8
sauritl <57330547+sauritl@users.noreply.github.com>
2022-05-26 21:57:14 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import streamlit as st
+import seaborn as sns
+import pandas as pd
+from gsheetsdb import connect
+
+
+
+
+
+
+
+st.title("Connect to Google Sheets")
+gsheet_url = st.secrets["public_gsheets_url"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{gsheet_url}"')
+df_gsheet = pd.DataFrame(rows)
+st.write(df_gsheet)
+
+
+t_array = df_gsheet['Nombre_Completo'].to_numpy()
+
+st.title("Ver el select")
+
+user = st.multiselect("Seleccione Usuario", t_array)
+
+
+st.title("Data Exploration")
+
+
+select = st.selectbox("Choose a variable for the x-axis", t_array)
+
+st.write(select)
+
+
+st.title("Connect to users")
+spotify_csv = st.secrets["spotify_csv"]
+conn = connect()
+rows = conn.execute(f'SELECT * FROM "{spotify_csv}"')
+spotify_csv = pd.DataFrame(rows)
+st.write(spotify_csv)
